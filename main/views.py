12331d8bb29cfffce9c0420f7a6a9ab4d4476a5c
Fredo Melvern Tanzil <fredotanzil@gmail.com>
2023-10-21 12:56:29 +0700

    Redeploy

--- main/views.py
+++ main/views.py
@@ -12,7 +12,6 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
 def register(request):
     form = UserCreationForm()
 
